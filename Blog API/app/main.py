f1edacba7747524e9180280251b57e442cb3712d
Jamezfn <jamesmakachia19@gmail.com>
2026-03-17 19:00:55 +0300

    CORS implementation

--- Blog API/app/main.py
+++ Blog API/app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from contextlib import asynccontextmanager
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.core.database import init_db
 from app.routers import auth, user, post, comment
@@ -12,6 +13,14 @@ async def lifespan(app: FastAPI):
     yield
 
 app = FastAPI(lifespan=lifespan)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(router=auth.router, prefix="/api/v1")
 app.include_router(router=user.router, prefix="/api/v1")
